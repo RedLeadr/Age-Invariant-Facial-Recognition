e65e3cb792a2dcaad1b807efb26bc1324b115229
Joseph Woodall <56406362+RedLeadr@users.noreply.github.com>
2020-09-25 10:13:54 -0500

    Initial libraries import

---
+++ libraries/feature_engineering.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 09:54:15 2020
+
+@author: josephwoodall
+"""
+
---
+++ libraries/misc_to_do.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 09:54:56 2020
+
+@author: josephwoodall
+"""
+
+'''
+
+Experiments folder - one for you and one for Kevin (he will make it) Jupiter notebooks if used
+Libraries - put main scripts 
+
+Establish scripts for each step in process (oop, custom modules) (try to write tools from scratch if you can, it looks good)
+
+Rewrite etl to python script
+
+Share drive link to Kevin once you have notion access
+
+
+'''
