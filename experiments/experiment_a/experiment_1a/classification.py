236264ec215293788ff3617bbc13da68f127797d
Joseph Woodall <josephlw4@gmail.com>
2020-10-28 16:52:04 -0500

    adds prelim for class CNN_SVM for programming tomorrow

--- experiments/experiment_a/experiment_1a/classification.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 13 13:26:52 2020
-
-@author: Joseph Woodall
-@author_email: josephlw4@gmail.com
-"""
-
---
+++ experiments/experiment_a/experiment_2a.py
@@ -0,0 +1,54 @@
+'''
+architecture:
+
+convolution -> pooling sub-sampling -> convolution -> pooling sub-sampling-> convolution -> fully connected -> fully connected
+
+from scratch
+'''
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+import os
+import tensorflow as tf
+import time
+import sys
+
+
+class CNN_SVM():
+
+    def __init__(self):
+        '''
+        Initializes the cnn-svm model
+        '''
+    
+    def model():
+        '''
+        builds model base
+        '''
+    
+    def train():
+        '''
+        trains model
+        '''
+    
+    def weightVariable():
+        '''
+        returns weight matrix with arbitrary values
+        '''
+    
+    def biasVariable():
+        '''
+        returns a bias matrix with 0.1 values
+        '''
+    
+    def conv2d(features, weight):
+        '''
+        produces a convolution layer that filters image subregion
+        '''
+    
+    def maxPool2x2(features):
+        '''
+        downsamples the image based on convolution layer
+        '''
+        
